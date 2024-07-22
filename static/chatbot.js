0a2ba460e9202cfd7f258583f0feddfc4283d3b1
RainVina <ranjohnvina@gmail.com>
2024-07-22 17:23:45 +0800

    Update2

--- static/chatbot.js
+++ static/chatbot.js
@@ -149,9 +149,12 @@ class Chatbox {
         // Reinsert the FAQ buttons
         const faqHtml = `
             <div class="chatbox__faq">
-                <button class="faq__button" data-question="How do I contact Dr. Suzy?">How do I contact Dr. Suzy?</button>
-                <button class="faq__button" data-question="When are you available?">When are you available?</button>
-                <button class="faq__button" data-question="How much is an appointment?">How much is an appointment?</button>
+                <button class="faq__button" data-question="How can I contact Dr. Suzy Roxas?">How can I contact Dr. Suzy Roxas?</button>
+                <button class="faq__button" data-question="What services are provided?">What services are provided?</button>
+                <button class="faq__button" data-question="What are the session fees?">What are the session fees?</button>
+                <button class="faq__button" data-question="When is Dr. Suzy Roxas available for appointments?">When is Dr. Suzy Roxas available for appointments?</button>
+                <button class="faq__button" data-question="Are there any internship opportunities?">Are there any internship opportunities?</button>
+                <button class="faq__button" data-question="Do you offer online therapy sessions?">Do you offer online therapy sessions?</button>
             </div>
         `;
         chatmessage.insertAdjacentHTML('afterbegin', faqHtml);
